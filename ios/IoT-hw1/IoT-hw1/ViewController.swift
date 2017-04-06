0b62a31acd9b21376297b5148d3699827f85569c
Yankai (Gerald) Zhang <monocofe@gmail.com>
2017-04-05 21:21:32 -0700

    Upload hw2 to git repo

---
+++ ios/IoT-hw1/IoT-hw1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IoT-hw1
+//
+//  Created by tshMacPro on 26/03/2017.
+//  Copyright © 2017 tshMacPro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
